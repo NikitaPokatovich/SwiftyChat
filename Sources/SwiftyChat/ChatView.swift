3512ecb10252207e839117f6bb7cf9d8e834fd3f
Nikita <me22anine@yandex.ru>
2021-11-28 14:04:38 +0300

    Unflip message

--- Sources/SwiftyChat/ChatView.swift
+++ Sources/SwiftyChat/ChatView.swift
@@ -71,7 +71,7 @@ public struct ChatView<Message: ChatMessage, User: ChatUser>: View {
             ScrollViewReader { proxy in
                 LazyVStack {
                     ForEach(messages) { message in
-                        chatMessageCellContainer(in: geometry.size, with: message).flippedUpsideDown()
+                        chatMessageCellContainer(in: geometry.size, with: message)
                     }
                     Spacer()
                         .frame(height: inset.bottom)
